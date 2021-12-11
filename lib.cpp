65847b481f8fa5cc0385001757cff09f516e39bc
5209482 <91668718+5209482@users.noreply.github.com>
2021-12-11 15:47:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -6,7 +6,7 @@
 float numeroMassimo( const float numeri[], int dim){
   numeri [dim] ;
   float massimo = numeri[0];
-  for (int i=0 ; i < dim ; i++){
+  for (int i = 0 ; i < dim ; i++){
     if(massimo < numeri[i]{
         massimo = numeri[i];
     }
