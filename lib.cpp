d45d4c3746f48a9957aa08dfde1496bf64eb35f7
5209482 <91668718+5209482@users.noreply.github.com>
2021-12-11 15:40:01 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,18 @@
+//
+// Created by Margherita Canale 
+//
+
 #include "lib.h"
 float numeroMassimo( const float numeri[], int dim){
   numeri [dim] ;
   float massimo = numeri[0];
-  fpr (int i=0 ; i<dim ; i++){
+  for (int i=0 ; i < dim ; i++){
     if(massimo < numeri[i]{
         massimo = numeri[i];
     }
        }
        return massimo;
        }
+      
       
     
