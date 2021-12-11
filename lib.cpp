4b83233f728dcdd839052eadf48733c1b9c801f5
5209482 <91668718+5209482@users.noreply.github.com>
2021-12-11 15:31:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+float numeroMassimo( const float numeri[], int dim){
+  numeri [dim];
+  float massimo = numeri[0];
+  fpr (int i=0; i<dim; i++){
+    if(massimo<numeri[i];
+       }
+       }
+       return massimo;
+       }
+    
